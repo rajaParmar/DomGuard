807ecca9472b12aadc1d28669493fe93ee956179
ANAND BHARARIA <anandbhararia@cse.iitb.ac.in>
2021-04-06 01:45:24 +0530

    front end done

--- websocketserver.py
+++ websocketserver.py
@@ -11,12 +11,14 @@ class SimpleEcho(WebSocket):
         # echo message back to client   
         sessionID = self.request.headers._headers[10][1]
         print("handle "+sessionID)
+        print(self.data)
 
     def connected(self):
         print(self.address, 'connected')
         sessionID = self.request.headers._headers[10][1]
         print("connect "+sessionID)
         sessionData[sessionID] = {"private" : token_bytes(32),"iv" : token_bytes(16), "decision" : False}
+        print(sessionData[sessionID])
         self.send_message(sessionData[sessionID]["private"].hex() + " " + sessionData[sessionID]["iv"].hex())
         print("connection success")
 
