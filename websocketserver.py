eb309aacb6cba557529fcf9222058703eeb018a8
ANAND BHARARIA <anandbhararia@cse.iitb.ac.in>
2021-04-06 22:27:20 +0530

    websocket server ready

--- websocketserver.py
+++ websocketserver.py
@@ -1,30 +1,38 @@
 from simple_websocket_server import WebSocketServer, WebSocket
 from secrets import token_bytes
-import hmac, hashlib
+import hmac, hashlib, re
 
 def getsha256hmac(key, message):
-    return hmac.new(key, message, hashlib.sha256).hesdigest()
+    return hmac.new(key, message, hashlib.sha256).hexdigest()
+    
+def clean(string):
+    return re.sub("[\'\"\\n\\r\\s]","",string)
+
+def generatePID(filename):
+    file = open(filename)
+    line = file.read()
+    mutation = '2222222222222222211'
+    pid = mutation + clean(line)
+    return pid
 
 sessionData = {}
-class SimpleEcho(WebSocket):
+class DomGuard(WebSocket):
     def handle(self):
-        # echo message back to client   
         sessionID = self.request.headers._headers[10][1]
-        print("handle "+sessionID)
-        print(self.data)
+        pid = bytes(generatePID('index.html'), 'utf-8')
+        temp = getsha256hmac(sessionData[sessionID]["private"], pid)
+        print(self.data.hex() == temp)
 
     def connected(self):
         print(self.address, 'connected')
         sessionID = self.request.headers._headers[10][1]
-        print("connect "+sessionID)
-        sessionData[sessionID] = {"private" : token_bytes(32),"iv" : token_bytes(16), "decision" : False}
-        print(sessionData[sessionID])
-        self.send_message(sessionData[sessionID]["private"].hex() + " " + sessionData[sessionID]["iv"].hex())
+        sessionData[sessionID] = {"private" : token_bytes(32), "decision" : False}
+        self.send_message(sessionData[sessionID]["private"].hex())
         print("connection success")
 
     def handle_close(self):
         print(self.address, 'closed')
 
-server = WebSocketServer('', 9000, SimpleEcho)
+server = WebSocketServer('', 9000, DomGuard)
 server.serve_forever()
 
