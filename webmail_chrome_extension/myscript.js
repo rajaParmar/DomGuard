882dcf7202daaafa04a57a292dac78f3264e7b1d
abhishek95x@gmail.com <abhishek95x@gmail.com>
2021-04-10 17:18:26 +0530

    Added webmail chrome extension

---
+++ webmail_chrome_extension/myscript.js
@@ -0,0 +1,29 @@
+//$('form').submit(function() {
+//		    alert('hacked!!');
+//                    $("input[type=submit]", this).prop("disabled", !0);
+//                    f.clear_messages();
+//                    f.display_message("", "Loading");
+//                    
+// });
+
+var button = document.getElementById("rcmloginsubmit");
+//button.value="hi";
+
+var uname = document.getElementById("rcmloginuser").value;
+var upwd = document.getElementById("rcmloginpwd").value;
+function convert(){
+    
+    
+    alert("hi"+uname+upwd);
+    $("input[type=submit]", this).prop("disabled", !0);
+    f.clear_messages();
+    f.display_message("", "Loading");
+}
+
+
+button.addEventListener("click", convert);
+
+//document.addEventListener("DOMContentLoaded", function() {
+//    document.getElementById("rcmloginsubmit").addEventListener("click", convert);
+//}//);
+
